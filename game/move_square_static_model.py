ac52841ca1bb794e2d9692a71189038c9419db82
Gus De Souza <gustavo.desouza@ocado.com>
2024-12-18 17:48:46 -0500

    add path for model

--- game/move_square_static_model.py
+++ game/move_square_static_model.py
@@ -2,8 +2,10 @@ from util.game import Game
 from util.inputs import FromModel
 from util.coordinate import Coordinate
 
+ML_MODEL = 'demonstrations_20241218_172133.pth'
+
 if __name__=="__main__":
-    game = Game(input=FromModel('model.pth', Coordinate(360,480), Coordinate(0,0)))
+    game = Game(input=FromModel(ML_MODEL, Coordinate(360,480), Coordinate(0,0)))
     game.start()
     game.quit()
     quit()

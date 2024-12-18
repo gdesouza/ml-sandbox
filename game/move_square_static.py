a4b2ae6a1c7f8ee3126d6f0e8ec38d622453ffbe
Gus De Souza <gustavo.desouza@ocado.com>
2024-12-18 17:49:21 -0500

    Save data into a csv file

--- game/move_square_static.py
+++ game/move_square_static.py
@@ -1,7 +1,14 @@
+from datetime import datetime
 from util.game import Game
 
 if __name__=="__main__":
-    game = Game()
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"demonstrations_{timestamp}.csv"
+
+    out = open(filename, 'w')
+    print('Execution,clock,current_position_x,current_position_y,target_position_x,target_position_y,move_x,move_y', file=out)
+    game = Game(output=out)
     game.start()
+    out.close()
     game.quit()
     quit()
